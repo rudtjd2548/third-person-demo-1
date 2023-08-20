64a340f3578b6ebcd10ec957d527220124e2f578
xy1131 <rudtjd2548@letsee.io>
2023-08-21 02:08:55 +0900

    [fix] euler 잔 수정

--- src/components/quest/ScrollEuler.tsx
+++ src/components/quest/ScrollEuler.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useRef } from 'react'
 import { useAppSelector } from '@src/redux/store.ts'
 
 const lineCount: number = 41
@@ -7,24 +7,21 @@ export default function ScrollEuler() {
   const { scroll }: any = useAppSelector((state) => state.quest)
   const bar = useRef<HTMLDivElement>(null!)
 
-  useEffect(() => {
-    bar.current.style.left = scroll.percent * 100 + '%'
-  }, [scroll])
-
   return (
     <div className='absolute left-[50%] bottom-5 transform translate-x-[-50%]'>
       <div className='w-[30vmin] flex'>
         {[...Array(lineCount)].map((_, index) => {
           const isEdge = index === 0 || index === lineCount - 1
           return (
-            <div key={index} className={`relative w-${index === lineCount - 1 ? '0' : 'full'}`}>
-              <div className={`absolute bottom-0 w-[2px] h-[${isEdge ? 1 : 0.5}rem] bg-[#00ffaa30]`} />
+            <div key={index} className={`relative ${index === lineCount - 1 ? 'w-0' : 'w-full'}`}>
+              <div className={`absolute bottom-0 w-[2px] ${isEdge ? 'h-[1rem]' : 'h-[0.5rem]'} bg-[#00ffaa30]`} />
             </div>
           )
         })}
         <div
           ref={bar}
-          className='absolute left-[0%] bottom-[100%] w-[4px] h-[1rem] transform translate-x-[-1px] rounded-2xl bg-[#ffffff] opacity-50'
+          style={{ left: scroll.percent * 100 + '%' }}
+          className='absolute bottom-[100%] w-[4px] h-[1rem] transform translate-x-[-1px] rounded-2xl bg-[#ffffff] opacity-50'
         />
       </div>
     </div>
